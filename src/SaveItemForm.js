4822f42b5329030f45fa3b2bf7d2b5e7b0836f1f
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-03-30 10:39:38 -0400

    new folder form

--- src/SaveItemForm.js
+++ src/SaveItemForm.js
@@ -1,56 +1,80 @@
-import React, {useEffect, useState} from "react"
-import Item from "./Item"
+import React, { useEffect, useState } from "react";
+import Item from "./Item";
 
-function SaveItemForm({thing, api, user }) {
-    const [formShowing, toggleForm] = useState(false)
-    const [folderId, setFolderId] = useState(null)
-    const [menuFolders, setMenuFolders] = useState([])
+function SaveItemForm({ thing, api, user }) {
+  const [formShowing, toggleForm] = useState(false);
+  const [folderId, setFolderId] = useState(null);
+  const [menuFolders, setMenuFolders] = useState([]);
+  const [showNewFolderInput, setShowNewFolderInput] = useState(false);
 
-    useEffect(()=> {
-      fetch(`${api}/users/${user.id}/saveditems`)
-        .then(r =>r.json())
-        .then(savedFolders => {
-          console.log(savedFolders)
-          setMenuFolders(savedFolders)
-          setFolderId(savedFolders[0].id)
-        })
-    }, [])
-
-    const handleSaveItem = (event) => {
-      console.log(event.target.value)
-        event.preventDefault()
-        console.log("u saved")
-        fetch(`${api}/items`, {
-          method: 'POST',
-          headers: {
-            'Content-Type':'application/json',
-            Accept:'application/json'
-          }, 
-          body: JSON.stringify({...thing, folder_id:folderId })
-        })
-      .then(r => r.json())
-      .then(item => alert(`${item.name} saved!`))
-      toggleForm(false)
+  useEffect(() => {
+    if (folderId === "newfolder") {
+      setShowNewFolderInput(true);
+    } else {
+      setShowNewFolderInput(false);
     }
+  }, [folderId]);
 
-    return (
-      <>
-        <button onClick={() => toggleForm(!formShowing)}>
-          Save Item?
-        </button>
-  
-        <form className={formShowing ? null :"hidden"} onSubmit={handleSaveItem}>
-            <select name="folder" value={folderId} onChange={e=>setFolderId(e.target.value)}>
-              {menuFolders.map(folder => <option key={folder.id} value={folder.id}>{folder.name}</option>)}
-              {/* <option value="newfolder" onSelect={e => console.log(e.target.value)}>New Folder</option> */}
-            </select>
-            <button type="submit">Save to Selected Folder</button>
-        </form>
-        <form>
+  useEffect(() => {
+    fetch(`${api}/users/${user.id}/saveditems`)
+      .then((r) => r.json())
+      .then((savedFolders) => {
+        console.log(savedFolders);
+        setMenuFolders(savedFolders);
+        setFolderId(savedFolders[0].id);
+      });
+  }, []);
 
-        </form>
-      </>
-    );
-  }
+  const handleSaveItem = (event) => {
+    console.log(event.target.value);
+    event.preventDefault();
+    console.log("u saved");
+    fetch(`${api}/items`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ ...thing, folder_id: folderId }),
+    })
+      .then((r) => r.json())
+      .then((item) => alert(`${item.name} saved!`));
+    toggleForm(false);
+  };
 
-  export default SaveItemForm;
+  return (
+    <>
+      <button onClick={() => toggleForm(!formShowing)}>Save Item?</button>
+
+      <form className={formShowing ? null : "hidden"} onSubmit={handleSaveItem}>
+        <select
+          name="folder"
+          value={folderId}
+          onChange={(e) => setFolderId(e.target.value)}
+        >
+          {menuFolders.map((folder) => (
+            <option key={folder.id} value={folder.id}>
+              {folder.name}
+            </option>
+          ))}
+          <option
+            value="newfolder"
+            onSelect={(e) => console.log(e.target.value)}
+          >
+            New Folder
+          </option>
+        </select>
+        <input
+          type="text"
+          name="New-Folder-Name"
+          className={showNewFolderInput ? "" : "hidden"}
+          placeholder="New Folder Name"
+        ></input>
+        <button type="submit">Save to Selected Folder</button>
+      </form>
+      <form></form>
+    </>
+  );
+}
+
+export default SaveItemForm;
